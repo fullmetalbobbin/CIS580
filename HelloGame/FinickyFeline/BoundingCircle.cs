52e58dae6cc4c40998c5eee313bb0d8a37872222
akdreesen <akdreesen@E1116W16>
2022-02-11 14:52:42 -0600

    BoundingCircle.cs added and cleanign up mess

---
+++ HelloGame/FinickyFeline/BoundingCircle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinickyFeline
+{
+    public struct BoundingCircle
+    {
+        public float X;
+
+        public float Y;
+
+        public float Radius;
+
+        public BoundingCircle(float x, float y, float radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+    }
+}
--- HelloGame/FinickyFeline/FinickyFelineGame.cs
+++ HelloGame/FinickyFeline/FinickyFelineGame.cs
@@ -16,23 +16,11 @@ namespace FinickyFeline
         private MouseSprite mouseSprite;
         private Texture2D title;
         private SpriteFont dosis;
-
-        //private Texture2D kibbles;
         private Kibble[] kibbles;
-
-        //private Texture2D shrimp;
         private Shrimp[] shrimps;
-
-        //private Texture2D beef;
         private Beef[] beefs;
-
-        //private Texture2D salmon;
         private Salmon[] salmons;
-
-        //private Texture2D tuna;
         private Tuna[] tunas;
-
-        //private Texture2D chicken;
         private Chicken[] chickens;
 
 
@@ -84,8 +72,6 @@ namespace FinickyFeline
                     new Chicken(new Vector2(628, 20)),
                     new Chicken(new Vector2(628, 370))
                 };
-            //chicken = new Chicken(new Vector2(628, 20));
-            //chicken = new Chicken(new Vector2(628, 370));
 
             base.Initialize();
         }
@@ -107,14 +93,6 @@ namespace FinickyFeline
             foreach(var salmon in salmons) salmon.LoadContent(Content);
             foreach(var tuna in tunas) tuna.LoadContent(Content);
             foreach(var chicken in chickens) chicken.LoadContent(Content);
-
-            //kibbles = Content.Load<Texture2D>("foodspritesheet");
-            //shrimp = Content.Load<Texture2D>("foodspritesheet");
-            //beef = Content.Load<Texture2D>("foodspritesheet");
-            //salmon = Content.Load<Texture2D>("foodspritesheet");
-            //tuna = Content.Load<Texture2D>("foodspritesheet");
-            //chicken = Content.Load<Texture2D>("foodspritesheet");
-
         }
 
         protected override void Update(GameTime gameTime)
@@ -137,41 +115,13 @@ namespace FinickyFeline
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            //cloveSprite.Draw(gameTime, spriteBatch);
-            //voomSprite.Draw(gameTime, spriteBatch);
             spriteBatch.Draw(title, new Vector2(120, 100), Color.White);
-
             foreach (var kibble in kibbles) kibble.Draw(gameTime, spriteBatch);
             foreach (var shrimp in shrimps) shrimp.Draw(gameTime, spriteBatch);
             foreach (var beef in beefs) beef.Draw(gameTime, spriteBatch);
             foreach (var salmon in salmons) salmon.Draw(gameTime, spriteBatch);
             foreach (var tuna in tunas) tuna.Draw(gameTime, spriteBatch);
             foreach (var chicken in chickens) chicken.Draw(gameTime, spriteBatch);
-
-            /*
-            kibbles.Draw(gameTime, spriteBatch);
-            shrimps.Draw(gameTime, spriteBatch);
-            beefs.Draw(gameTime, spriteBatch);
-            salmons.Draw(gameTime, spriteBatch);
-            tuna.Draw(gameTime, spriteBatch);
-            chicken.Draw(gameTime, spriteBatch);
-            */
-            /*
-            spriteBatch.Draw(kibbles, new Vector2(58, 20), new Rectangle(0, 0, 64, 64), Color.White);
-            spriteBatch.Draw(shrimp, new Vector2(172, 20), new Rectangle(64, 64, 64, 64), Color.White);
-            spriteBatch.Draw(beef, new Vector2(286, 20), new Rectangle(128, 64, 64, 64), Color.White);
-            spriteBatch.Draw(salmon, new Vector2(400, 20), new Rectangle(128, 0, 64, 64), Color.White);
-            spriteBatch.Draw(tuna, new Vector2(514, 20), new Rectangle(64, 0, 64, 64), Color.White);
-            spriteBatch.Draw(chicken, new Vector2(628, 20), new Rectangle(0, 64, 64, 64), Color.White);
-            spriteBatch.Draw(kibbles, new Vector2(58, 370), new Rectangle(0, 0, 64, 64), Color.White);
-            spriteBatch.Draw(shrimp, new Vector2(172, 370), new Rectangle(64, 64, 64, 64), Color.White);
-            spriteBatch.Draw(beef, new Vector2(286, 370), new Rectangle(128, 64, 64, 64), Color.White);
-            spriteBatch.Draw(salmon, new Vector2(400, 370), new Rectangle(128, 0, 64, 64), Color.White);
-            spriteBatch.Draw(tuna, new Vector2(514, 370), new Rectangle(64, 0, 64, 64), Color.White);
-            spriteBatch.Draw(chicken, new Vector2(628, 370), new Rectangle(0, 64, 64, 64), Color.White);
-            
-            spriteBatch.DrawString(dosis, "The Finicky Feline", new Vector2(300, 110), Color.Black);
-            */
             spriteBatch.DrawString(dosis, "Exit game - Press Back or ESC", new Vector2(GraphicsDevice.Viewport.Width/3  + 10,GraphicsDevice.Viewport.Height - 30), Color.Black );
             mouseSprite.Draw(gameTime, spriteBatch);
             cloveSprite.Draw(gameTime, spriteBatch);
