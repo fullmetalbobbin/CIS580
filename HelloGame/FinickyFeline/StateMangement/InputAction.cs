0ee2d3d174d12308ec0dfd61fb626da25c116466
akdreesen <akdreesen@E1117W22>
2022-02-25 16:30:30 -0600

    Attempting to restructure and shift input

---
+++ HelloGame/FinickyFeline/StateMangement/InputAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace FinickyFeline.StateMangement
+{
+    public class InputAction
+    {
+        private bool firstPress;
+
+        private Keys[] keys;
+        private Buttons[] buttons;
+
+        public InputAction(bool first, Keys[] pressedKeys, Buttons[] pressedButtons)
+        {
+            firstPress = first;
+            keys = pressedKeys != null ? pressedKeys.Clone() as Keys[]: new Keys[0];
+            buttons = pressedButtons != null ? pressedButtons.Clone() as Buttons[] : new Buttons[0];
+        }
+
+        public bool InputOccured(InputState inputState)
+        {
+            Keys keyTest;
+            Buttons buttonTest;
+
+            if (firstPress)
+            {
+                keyTest = inputState
+            }
+            return false;
+        }
+    }
+}
--- HelloGame/FinickyFeline/StateMangement/InputState.cs
+++ HelloGame/FinickyFeline/StateMangement/InputState.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace FinickyFeline.StateMangement
 {
-    class InputState
+    public class InputState
     {
+        // Initalizing for gamepad input
+        public readonly GamePadState CurrentGamePadState;
+        private readonly GamePadState previousGamePadState;
+
+        // Initializing for keyboard input
+        public readonly KeyboardState CurrentKeyboardState;
+        private readonly KeyboardState previousKeyboardState;
+
+        public InputState()
+        {
+            CurrentGamePadState = new GamePadState();
+            previousGamePadState = new GamePadState();
+
+            CurrentKeyboardState = new KeyboardState();
+            previousKeyboardState = new KeyboardState();
+
+        }
     }
 }
